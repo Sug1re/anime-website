0366cd3d690ca1ad4a267e31056d6a14c15cecc0
Sug1re <sugirex@icloud.com>
2024-12-20 14:21:04 +0900

    Footer完成

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -190,7 +190,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("af57990bc06a7a12")
+/******/ 		__webpack_require__.h = () => ("a301229808d02fef")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: /Users/sakura/anime-website/app/page.tsx
+// File: /Users/sugiyamadaichi/anime-website/app/page.tsx
 import * as entry from '../../../app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
