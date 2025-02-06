23318e145dfe23ac90fb8683f913d5dfbeccfc21
usukisakura <sakura@24N1102358usukisakura.local>
2025-02-06 12:28:40 +0900

    test

--- app/Components/Cast/Cast.tsx
+++ app/Components/Cast/Cast.tsx
@@ -14,31 +14,31 @@ const Cast = () => {
       <div className=" h-screen w-screen flex flex-col items-center justify-center">
         <div className=" grid grid-cols-3 gap-x-24 gap-y-6">
           <div className=" flex flex-col">
-            <p className=" md:text-lg font-semibold text-red-600">原作</p>
+            <p className=" md:text-lg font-semibold text-blue-500">原作</p>
             <p className=" text-lg md:text-1xl font-bold">オダギリ ハジメ</p>
           </div>
           <div className=" flex flex-col">
-            <p className=" md:text-lg font-semibold text-red-600">監督</p>
+            <p className=" md:text-lg font-semibold text-blue-500">監督</p>
             <p className=" text-lg md:text-1xl font-bold">遠藤 義信</p>
           </div>
           <div className=" flex flex-col">
-            <p className=" md:text-lg font-semibold text-red-600">
+            <p className=" md:text-lg font-semibold text-blue-500">
               シリーズ構成・脚本
             </p>
             <p className=" text-lg md:text-1xl font-bold">小西 仁美</p>
           </div>
           <div className=" flex flex-col">
-            <p className=" md:text-lg font-semibold text-red-600">
+            <p className=" md:text-lg font-semibold text-blue-500">
               キャラクターデザイン
             </p>
             <p className=" text-lg md:text-1xl font-bold">井口 澪</p>
           </div>
           <div className=" flex flex-col">
-            <p className=" md:text-lg font-semibold text-red-600">総作画監督</p>
+            <p className=" md:text-lg font-semibold text-blue-500">総作画監督</p>
             <p className=" text-lg md:text-1xl font-bold">高野 源一</p>
           </div>
           <div className=" flex flex-col">
-            <p className=" md:text-lg font-semibold text-red-600">色彩設計</p>
+            <p className=" md:text-lg font-semibold text-blue-500">色彩設計</p>
             <p className=" text-lg md:text-1xl font-bold">渡部 みどり</p>
           </div>
         </div>
