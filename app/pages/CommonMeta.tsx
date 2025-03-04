a3e31af7b623285d64f386b93255595ecbe1e656
Sug1re <sugirex@icloud.com>
2025-03-05 01:17:13 +0900

    Metadataの 記入

--- app/pages/CommonMeta.tsx
+++ app/pages/CommonMeta.tsx
@@ -1,30 +1,44 @@
-// import Head from "next/head";
+import Head from "next/head";
 
-// type Props = {
-//   title: string;
-//   description: string;
-//   url: string;
-//   type: string;
-//   imageUrl: string;
-// };
+type Props = {
+  title: string;
+  description: string;
+  url: string;
+  type: string;
+  imageUrl: string;
+};
 
-// const CommonMeta = (props: Props) => {
-//   const { title, description, url, type, imageUrl } = props;
-//   return (
-//     <Head>
-//       <title>{"TVアニメ「描く光、描く影」公式サイト"}</title>
-//       <meta name="description" content={description} />
-//       <meta name="twitter:card" content="summary_large_image" />
-//       <meta property="og:title" content="ページのタイトル" />
-//       <meta property="og:description" content="ページの説明" />
-//       <meta property="og:image" content="https://example.com/og-image.jpg" />
-//       <meta property="og:url" content="https://example.com" />
-//       <meta
-//         property="og:site_name"
-//         content="TVアニメ「描く光、描く影」公式サイト"
-//       />
-//       <meta property="og:type" content="website" />
-//     </Head>
-//   );
-// };
-// export default CommonMeta;
+const CommonMeta = (props: Props) => {
+  const { title, description, url, type, imageUrl } = props;
+  return (
+    <Head>
+      <title>{"TVアニメ「描く光、描く影」公式サイト"}</title>
+      <meta
+        name="description"
+        content={
+          "学内コンペでWeb開発部門に応募した作品です。6人で活動してイラスト担当とWeb担当に分担して作業しました。*空想のアニメなのでTVやサブスクでは放送されません。*"
+        }
+      />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta
+        property="og:title"
+        content="TVアニメ「描く光、描く影」公式サイト"
+      />
+      <meta
+        property="og:description"
+        content="学内コンペでWeb開発部門に応募した作品です。6人で活動してイラスト担当とWeb担当に分担して作業しました。*空想のアニメなのでTVやサブスクでは放送されません。*"
+      />
+      <meta
+        property="og:image"
+        content="https://anime-website-sage.vercel.app/keyVisual.png"
+      />
+      <meta property="og:url" content="https://anime-website-sage.vercel.app" />
+      <meta
+        property="og:site_name"
+        content="TVアニメ「描く光、描く影」公式サイト"
+      />
+      <meta property="og:type" content="website" />
+    </Head>
+  );
+};
+export default CommonMeta;
