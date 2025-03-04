7c506a1aca998afd9baf8e7d345982a0f4403adf
Sug1re <sugirex@icloud.com>
2025-03-05 01:00:44 +0900

    Metadataの 記入

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,13 +24,6 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="ja">
-      <head>
-        <title>TVアニメ「描く光、描く影」公式サイト</title>
-        <meta
-          name="description"
-          content="学内コンペでWeb開発部門に応募した作品です。6人で活動してイラスト担当とWeb担当に分担して作業しました。「東京郊外の桜丘高校を舞台に、絵を描くことに情熱を注ぐユウと、美術部の天才アヤが織りなす青春ストーリー 絵を通じて未来に向かっていく青春物語が今、始まる!?」＊空想のアニメなのでTVやサブスクでは放送されません。"
-        />
-      </head>
       <body className="antialiased flex">
         <main>{children}</main>
       </body>
