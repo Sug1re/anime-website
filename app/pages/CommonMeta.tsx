9c58081dc300dbe2a875684841e417b07219bc85
Sug1re <sugirex@icloud.com>
2025-03-05 01:03:07 +0900

    Metadataの 記入

--- app/pages/CommonMeta.tsx
+++ app/pages/CommonMeta.tsx
@@ -1,30 +1,30 @@
-import Head from "next/head";
+// import Head from "next/head";
 
-type Props = {
-  title: string;
-  description: string;
-  url: string;
-  type: string;
-  imageUrl: string;
-};
+// type Props = {
+//   title: string;
+//   description: string;
+//   url: string;
+//   type: string;
+//   imageUrl: string;
+// };
 
-const CommonMeta = (props: Props) => {
-  const { title, description, url, type, imageUrl } = props;
-  return (
-    <Head>
-      <title>{"TVアニメ「描く光、描く影」公式サイト"}</title>
-      <meta name="description" content={description} />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta property="og:title" content="ページのタイトル" />
-      <meta property="og:description" content="ページの説明" />
-      <meta property="og:image" content="https://example.com/og-image.jpg" />
-      <meta property="og:url" content="https://example.com" />
-      <meta
-        property="og:site_name"
-        content="TVアニメ「描く光、描く影」公式サイト"
-      />
-      <meta property="og:type" content="website" />
-    </Head>
-  );
-};
-export default CommonMeta;
+// const CommonMeta = (props: Props) => {
+//   const { title, description, url, type, imageUrl } = props;
+//   return (
+//     <Head>
+//       <title>{"TVアニメ「描く光、描く影」公式サイト"}</title>
+//       <meta name="description" content={description} />
+//       <meta name="twitter:card" content="summary_large_image" />
+//       <meta property="og:title" content="ページのタイトル" />
+//       <meta property="og:description" content="ページの説明" />
+//       <meta property="og:image" content="https://example.com/og-image.jpg" />
+//       <meta property="og:url" content="https://example.com" />
+//       <meta
+//         property="og:site_name"
+//         content="TVアニメ「描く光、描く影」公式サイト"
+//       />
+//       <meta property="og:type" content="website" />
+//     </Head>
+//   );
+// };
+// export default CommonMeta;
